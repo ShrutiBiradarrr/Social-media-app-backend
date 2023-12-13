3db025f915f63bd1b3d84ec7a4c792db417d6525
shrutii1010 <shruti.biradar818@gmail.com>
2023-12-13 16:54:15 +0530

    mongoose updated

--- database/connection.js
+++ database/connection.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 
 
 const database = "mongodb+srv://shrutibiradar818:eJcCSJJqAiXfKpog@cluster1.50ig38a.mongodb.net/dbuser?retryWrites=true&w=majority";
-const dbConnection = async()=>{
+const dbConnection = ()=>{
 
-    
     mongoose.connect(`${database}`)
     .then(() => console.log("Database succesfully connected with server..."))
     .catch((err) => console.log(err));
